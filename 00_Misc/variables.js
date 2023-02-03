ec38d2fc30c666e09c739c09d07a9c65ffda6c6e
tobiasras <tobiasras.job@gmail.com>
2023-02-03 10:30:53 +0100

    init

---
+++ 00_Misc/variables.js
@@ -0,0 +1,15 @@
+// const Rule 1: const me; u have to assign a value
+
+
+const me = {};
+
+// const Rule 2: cannot reasign to constant
+
+const hobbies = ["cooking", "football"];
+
+
+const hobbyOne = '"asdkjaldsalkdsjasldkj"'
+const hobbyTwo = "asdajsd 'asdaskdhjadskjhsa' "
+const hobbyThree = `'asdljasdlkjasdlkjs' "laskjd" lkasjdlksa jd ${ 2 + 2 }`
+
+console.log(hobbyThree)
